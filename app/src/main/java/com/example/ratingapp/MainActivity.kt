f4c1c54ed6e3d66f41c0100a6d13bd849726a1fb
Ericgacoki <gacokieric@gmail.com>
2021-05-03 12:17:07 +0300

    Implemented data binding
    Signed-off-by: Ericgacoki <gacokieric@gmail.com>

--- app/src/main/java/com/example/ratingapp/MainActivity.kt
+++ app/src/main/java/com/example/ratingapp/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.example.ratingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.RatingBar
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ratingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // use getter to get non-null value for binding variable
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            val rating = rating.toString()
+        captureRating()
+    }
 
-            tvRating.text = rating
+    @SuppressLint("SetTextI18n") // ignore concatenation warning
+    private fun captureRating() {
+        var userRating = 0F // initial value
+
+        // use _ for unused parameters in this lambda function
+        binding.ratingBar.apply {
+            setOnRatingBarChangeListener { _, rating, _ ->
+                userRating = rating.also {
+                    binding.tvRating.text = "${it.toInt()}/5"
+                }
+            }
         }
 
-        send.setOnClickListener {
-            etSaySomething.text?.clear()
-
-            Toast.makeText(applicationContext, "Your Review is Submitted To ERIC G.", Toast.LENGTH_SHORT).show()
+        binding.btnSend.setOnClickListener {
+            if (binding.etRating.text!!.isNotEmpty()) {
+                Toast.makeText(this, "Rating ($userRating/5.0) sent to Moha", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                binding.etRating.error = "can't be blank"
+            }
         }
-
     }
 }
